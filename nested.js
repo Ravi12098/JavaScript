055a1e04f74199db2305fb67e5fe8f5ce35b85b8
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-19 11:34:48 +0530

    Add files via upload

---
+++ nested.js
@@ -0,0 +1,12 @@
+let isEmailCorrect = true;
+let isOTPcorrect = false;
+
+if (isEmailCorrect) {
+    if (isOTPcorrect) {
+        console.log("Login Success");
+    } else {
+        console.log("Incorrect OTP");
+    }
+} else {
+    console.log("Email Incorrect");
+}
