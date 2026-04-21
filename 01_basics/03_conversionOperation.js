d37266ce2dbc72843de2c2899895d5c82de0e517
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-21 10:48:05 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,33 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ******************  Operations  **************
+
+let value = 3
+let nagValue = -value
+console.log(nagValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " Ravi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + "3" + 2);
+console.log(1 + 3 + "3");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
