f851b1c1c3858c93a87d3c4e4714c08d94201f57
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-21 08:25:13 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+let name = "Ravi"
+let age = 20
+let isLoggedIn = false
+let state;
+let cite = null
+
+/*
+number => 2 tp power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined 
+symbol => unique
+
+
+object
+
+*/
+
+console.log(typeof undefined); // output => undefined
+console.log(typeof null); // output => object
+
+console.log(typeof name);
+
