6a6258122cd42027c492e90fb347399a77778fd7
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-23 07:59:18 +0000

    date in js

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2026, 11, 25)
+console.log(myCreatedDate.toDateString());
+
+
+
