33c756a626ef3c8c56f61d597c013cdaa52a32a3
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-26 09:10:21 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+// scope = {}
+
+// var c = 100
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Ravi"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+one()
+
+if (true) {
+    const username = "Ravi"
+    if (username === "Ravi"){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(7))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
