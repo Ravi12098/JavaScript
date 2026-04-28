35bc4fd00e745d736f9a7a6274bb20aef41e321f
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-28 10:02:44 +0000

    for of loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,25 @@
+// for of 
+
+const arr = [1, 2, 3, 4]
+for (const num of arr) {
+    console.log(num);   
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(greet);
+}
+
+// Maps 
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ":-", value);
+    
+}
