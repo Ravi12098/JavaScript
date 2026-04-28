25275cf9911d5f148d9e741d2a208f65b44fb0fe
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-04-28 10:34:12 +0000

    foreach loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,36 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach( function (val){
+    console.log(val);
+    
+} )
+
+coding.forEach( (item) => {
+    console.log(item);
+    
+} )
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+    
+} )
+
+
+const myCoding = [
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {    
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {  
+        languageName: "javascript",
+        languageFileName: "js"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
