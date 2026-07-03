ceb82aaf1a667febe6f8c41c9de07add3af7ad1f
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-07-03 07:18:30 +0000

    object in js

---
+++ object/app.js
@@ -0,0 +1,22 @@
+const post = {
+    userName: "@ravinder",
+    content: "this is my #fristPost",
+    likes: 200,
+    reposts: 4,
+    tags: ["@monu", "@rahul"]
+}
+
+const classInfo = {
+    aman: {
+        grade: "A+",
+        city: "Pune"
+    },
+    ravi: {
+        grade: "A+",
+        city: "Jaipur"
+    },
+    monu: {
+        grade: "A",
+        city: "Mumbai"
+    }
+};
