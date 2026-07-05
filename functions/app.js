ac58eb95bd6e0fc354795c3989e55bf0e97f9dbe
Ravinder Saharan <163217684+Ravi12098@users.noreply.github.com>
2026-07-05 06:18:49 +0000

    function

---
+++ functions/app.js
@@ -0,0 +1,25 @@
+function name() {
+    console.log("Ravinder");
+}
+
+name();
+name();
+
+function num1to5() {
+    for ( let i = 1; i <= 5; i++){
+        console.log(i);
+    }
+}
+
+num1to5();
+
+function isAdult() {
+    let age = 18;
+    if(age >=18) {
+        console.log("adult");
+    }else {
+        console.log("not adult");
+    }
+}
+
+isAdult();
